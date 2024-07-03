997ed36be3ab13a3abd35d7bd348924b1973b131
Al Faiz Ali <alfaizali8410@gmail.com>
2024-07-04 02:25:48 +0530

    Commit

--- backend/middlewares/db.js
+++ backend/middlewares/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/FoodApp';
+const MONGODB_URI = process.env.MONGODB_URI;
 // MongoDB Connection
 export default await mongoose.connect(MONGODB_URI)
     .then(() => { console.log("db connected") })
